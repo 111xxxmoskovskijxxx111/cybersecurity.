9caa9594adf8e6aedd66adb5a2d8845197b9046f
111xxxmoskovskijxxx111 <dimamoskovskij@gmail.com>
2026-03-16 15:47:33 +0200

    Lab 3: Implemented Subresource Integrity protection

--- statichost-6000/server.js
+++ statichost-6000/server.js
@@ -1,11 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const PORT = 6000;
+    const express = require("express");
+    const cors = require("cors");
+    const app = express();
+    const PORT = 6000;
 
-app.use(cors());
-app.use(express.static(__dirname));
+    app.use(cors());
+    app.use(express.static(__dirname));
 
-app.listen(PORT, () => {
-    console.log("StaticHost running on http://localhost:6000");
-});
+    const mode = process.argv[2] || "normal";
+
+    app.get("/react-mock.js", (req, res) => {
+
+        if (mode === "breach") {
+            res.send('alert("CRITICAL: CDN Compromised! Stealing data...");');
+        } else {
+            res.send('console.log("React v1.0.0 loaded from CDN");');
+        }
+    
+    });
+
+    app.listen(PORT, () => {
+        console.log("StaticHost running on http://localhost:6000");
+    });
